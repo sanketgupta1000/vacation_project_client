7ff4b1898eeb096c0ed67f77ae0921bf3e312a0f
Sanket <sanketgupta1000@gmail.com>
2024-06-13 14:38:28 +0530

    added authService

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+const config = {
+    urlPrefix: "localhost:8080"
+}
+
+export default config
---
+++ src/services/authService.js
@@ -0,0 +1,82 @@
+import config from "../config/config";
+
+class AuthService
+{
+    // method to signup
+    async signup(email, password, fullName, phoneNumber)
+    {
+
+        // get data as obj
+        const signupData = {
+            email,
+            password,
+            fullName,
+            phoneNumber
+        }
+
+        // return the response promise
+        return fetch(
+            // url to send the request to
+            config.urlPrefix + "/auth/signup",
+            {
+                // method
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                // request body
+                body: JSON.stringify(signupData)
+            }
+        )
+    }
+
+    // method to verify otp
+    async verifyOtp(email, otp)
+    {
+
+        // first creating a form data
+        const formData = new FormData()
+        formData.append("email", email)
+        formData.append("otp", otp)
+
+        return fetch(
+            config.urlPrefix + "/auth/verify-otp",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                },
+                body: formData
+            }
+        )
+    }
+
+    // method to login
+    async login(email, password)
+    {
+
+        // data object
+        const loginData = {
+            email,
+            password
+        }
+
+        // fetching, will get jwt as response
+        return fetch(
+            config.urlPrefix + "/auth/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginData)
+            }
+        )
+        
+    }
+
+
+}
+
+const authService = new AuthService();
+export default authService;
