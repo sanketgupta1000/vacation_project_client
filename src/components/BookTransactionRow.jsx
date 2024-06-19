5cce378f8d79545ef5a58e6cbebf6e7ae31eb4a3
Janvi-1802 <janvivsolanki2004@gmail.com>
2024-06-19 11:15:42 +0530

    changes in bookcopy and booktransaction

---
+++ src/components/BookTransactionRow.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+function BookTransactionRow({BookTransactionDTO})
+{
+    return (
+        <>
+        <div>
+        <h3>{BookTransactionDTO.transactionId}</h3>
+        <h3>{BookTransactionDTO.bookId}</h3>
+        <h3>{BookTransactionDTO.bookCopyId}</h3>
+        <h3>{BookTransactionDTO.bookTitle}</h3>
+        <h3>{BookTransactionDTO.bookGiverFullName}</h3>
+        <h3>{BookTransactionDTO.bookGiverEmail}</h3>
+        <h3>{BookTransactionDTO.bookReceiverFullName}</h3>
+        <h3>{BookTransactionDTO.bookReceiverEmail}</h3>
+        <h3>{BookTransactionDTO.transactionDate}</h3>
+        <h3>{BookTransactionDTO.transactionTime}</h3>
+        </div>
+        </>
+    );
+}
+export default BookTransactionRow;
