148e2dce47ad5f6f18c8510a1a1b895423101e51
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-06-20 22:31:50 +0530

    Added Book Copy component

--- src/components/BookCopy.jsx
+++ src/components/BookCopy.jsx
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 
 import { bookService } from "../services"
-import { setInfo, setLoading } from "../slices"
+import { setInfo, setLoading, setSingleBookCopy } from "../slices"
 import { useEffect } from "react"
 
 const BookCopy = ()=>
@@ -16,6 +16,8 @@ const BookCopy = ()=>
     // token
     const jwt = useSelector(state=>state.userReducer.token)
 
+    const singleBookCopy = useSelector((state)=>state.bookCopyReducer.singleBookCopy)
+
     
     useEffect(()=>{
         const fetchData = async ()=>{
@@ -26,38 +28,15 @@ const BookCopy = ()=>
                 throw new Error(await response.json())
             }
 
-            const data = await response.json()
+            const singleBookCopy = await response.json()
 
-            const singleBookCopy = {
-                bookCopyId: data.bookCopyId,
-                bookId: data.bookId,
-                bookTitle: data.bookTitle,
-                holderId: null,
-                holderName: null,
-                borrowerId: null,
-                borrowerName: null,
-                requestable: null,
-                bookTransactions: [
-                    {
-                        transactionId: null,
-                        bookGiverId: null,
-                        bookGiverFullName: null,
-                        bookGiverEmail: null,
-                        bookReceiverId: null,
-                        bookReceiverFullName: null,
-                        bookReceiverEmail: null,
-                        transactionDate: null,
-                        transactionTime: null,
-                    }
-                ]
-            }
+            dispatch(setSingleBookCopy(singleBookCopy))
         }    
         dispatch(setLoading({isLoading: true, loadingMsg: 'Loading data...'}))
         try
         {   
-            
-
-            
+            fetchData()
+            navigate("/")
         }
         catch(error)
         {
@@ -65,10 +44,46 @@ const BookCopy = ()=>
         }
         finally
         {
-
+            dispatch(setLoading({isLoading: false, loadingMsg: 'Loading data...'}))
         }
     },[])
 
+    return(
+        <div>
 
+            {/* bookCopy details */}
+
+            <div className="flex flex-col md:flex-row bg-gray-100 p-4 rounded-lg shadow-md">
+                <div className="md:w-1/3">
+                    <h2 className="text-xl font-bold">{singleBookCopy.bookTitle}</h2>
+                    <p className="text-gray-500">Book Copy ID: {singleBookCopy.bookCopyId}</p>
+                    <p className="text-gray-500">Book ID: {singleBookCopy.bookId}</p>
+                </div>
+                <div className="md:w-2/3 flex flex-wrap justify-between mt-4 md:mt-0">
+                    <div className="flex items-center mb-2 md:mb-0">
+                        <span className="text-gray-500 mr-2">Holder</span>
+                        <span className="text-gray-700">{singleBookCopy.holderName}</span>
+                    </div>
+                    <div className="flex items-center mb-2 md:mb-0">
+                        <span className="text-gray-500 mr-2">Next will be passed to:</span>
+                        <span className="text-gray-700">{singleBookCopy.borrowerName}</span>
+                    </div>
+                </div>
+            </div>
+            {/* all copies of the book */}
+            <div>
+
+                {singleBookCopy.bookCopyTransactions.map((transaction)=>
+                (
+                    <BookTransactionRow
+                        key={transaction.transactionId}
+                        transaction={transaction}
+                    />
+                ))}
+
+            </div>
+
+        </div>
+    )
 
 }
